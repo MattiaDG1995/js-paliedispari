39e05cdbd17083a6e9deb412b6633e6a16450696
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-10-27 15:12:42 +0200

    fix palindromo

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+
+let parola = prompt('inserisci parola')
+let parolaReverse = '';
+
+function reverse(x){
+
+    for( let i = x.length - 1; i >= 0; i-- ){
+
+        parolaReverse += x[i]
+    }
+
+    if( parolaReverse == x ){
+        console.log('palindroma')
+
+    }else{
+        console.log('nope')      
+    }
+
+ }
+
+ reverse(parola)
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -9,22 +32,4 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-let parola = prompt('inserisci parola')
-let parolaReverse = '';
 
-function reverse(parola){
-
-    for( let i = parola.length - 1; i >= 0; i--){
-
-        parolaReverse += parola[i]
-    }
-
-    if( parolaReverse == parola ){
-        console.log('palindroma')
-
-    }else{
-        console.log('nope')
-        
-    }
- }
- reverse(parola)
